2bd007073074b43759d8fb895039b6b51e24555b
Илья Грачёв <ilyagrachev@MacBook-Pro-Ila.local>
2021-05-15 23:24:45 +0300

    Версия 1_2 15.05

--- MentorFinderBot.py
+++ MentorFinderBot.py
@@ -4,19 +4,16 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from settings import TG_TOKEN
-
+from telegram import ReplyKeyboardMarkup
 
 #функция parrot отвечает тем же сообщением, которое ему прислали (П:Привет! Б:Привет!)
 #функция sms вызывается пользователем при отправке команды, к которой эта функция привязана
-def sms(bot, update):
+def sms_start(bot, update):
     print('start')
     bot.message.reply_text('Привет, {}! \nТы здесь, потому что хочешь найти ментора или хочешь стать им. \nПозволь мне помочь!'.format(bot.message.chat.first_name))
     print(bot.message)
-
-
-def sms1(bot, update):
-    print('choose')
-    bot.message.reply_text('Выбери сферу')
+    keyboard_side = ReplyKeyboardMarkup([['Найти ментора'], ['Стать ментором']])
+    bot.message.reply_text('Хочешь найти ментора или стать ментором?', reply_markup=keyboard_side)
 
 
 
@@ -25,8 +22,7 @@ def sms1(bot, update):
 def main():
     my_bot = Updater(TG_TOKEN)
 
-    my_bot.dispatcher.add_handler(CommandHandler('start', sms))
-    my_bot.dispatcher.add_handler(CommandHandler('choose', sms1))
+    my_bot.dispatcher.add_handler(CommandHandler('start', sms_start))
 
     my_bot.start_polling()
     my_bot.idle()
